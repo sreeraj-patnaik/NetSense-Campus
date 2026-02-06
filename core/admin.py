95eb10a67b77a0e860fd0ec3a0ea6ac02ac75391
sreeraj-patnaik <sreerajscs16225@gmail.com>
2026-02-06 21:02:03 +0530

    updated models to show floor and block names neatly

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+# registering the models to be visible in the admin interface
+from .models import Block, Floor
+admin.site.register(Block)
+admin.site.register(Floor)
+
