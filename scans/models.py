908e0b319677c507dd400cd259e8086efcab43c1
sreeraj-patnaik <sreerajscs16225@gmail.com>
2026-02-06 22:33:02 +0530

    updated scan/models

--- scans/models.py
+++ scans/models.py
@@ -1,3 +1,37 @@
+
+
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from core.models import Floor
+
+class ScanRawWiFi(models.Model):
+
+    floor = models.ForeignKey(Floor, on_delete=models.CASCADE)
+
+    cell_id = models.IntegerField()
+
+    ssid = models.CharField(max_length=100)
+    bssid = models.CharField(max_length=50)
+
+    rssi = models.IntegerField()
+    frequency = models.IntegerField()
+
+    timestamp = models.DateTimeField()
+    device_id = models.CharField(max_length=100)
+
+class ScanRawMobile(models.Model):
+
+    floor = models.ForeignKey(Floor, on_delete=models.CASCADE)
+
+    cell_id = models.IntegerField()
+
+    provider = models.CharField(max_length=50)
+    network_type = models.CharField(max_length=10)
+
+    signal_strength = models.IntegerField()
+
+    timestamp = models.DateTimeField()
+    device_id = models.CharField(max_length=100)
