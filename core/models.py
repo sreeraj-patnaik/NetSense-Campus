9d0a17b8b429356fb360209812c9f8c7f4792c77
sreeraj-patnaik <sreerajscs16225@gmail.com>
2026-02-06 20:37:21 +0530

    installed pillow and updated requirements

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Block(models.Model):
+    name = models.CharField(max_length=50)
+
+class Floor(models.Model):
+    block = models.ForeignKey(Block, on_delete=models.CASCADE)
+    floor_number = models.IntegerField()
+    name = models.CharField(max_length=50)
+
+    map_image = models.ImageField(upload_to="floors/")
+    rows = models.IntegerField()
+    cols = models.IntegerField()
+
+    blocked_cells = models.JSONField(default=list)
