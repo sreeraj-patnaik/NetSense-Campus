7460cfed9066e72afb57aa2395d66de7fb236f3f
sreeraj-patnaik <sreerajscs16225@gmail.com>
2026-02-21 18:40:50 +0530

    chore(whole) : redoing entire thing

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-# registering the models to be visible in the admin interface
-from .models import Block, Floor
-admin.site.register(Block)
-admin.site.register(Floor)
-
