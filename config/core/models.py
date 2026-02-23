e96692402e2bc7b42e70e036cc9510cf955134cb
sreeraj-patnaik <sreerajscs16225@gmail.com>
2026-02-23 15:17:54 +0530

    fix(core/models) : changed Charfield -> CharField

--- config/core/models.py
+++ config/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Block(models.Model):
-    name = models.Charfield(max_length=100)
+    name = models.CharField(max_length=100)
     
     def __str__(self):
         return self.name
