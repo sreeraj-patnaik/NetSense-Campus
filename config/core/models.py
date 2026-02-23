da65029a77152b3995e1613f08506e36a31c28eb
sreeraj-patnaik <sreerajscs16225@gmail.com>
2026-02-23 15:14:57 +0530

    build(core/models) : Block and floor models written

--- config/core/models.py
+++ config/core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Block(models.Model):
+    name = models.Charfield(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+class Floor(models.Model):
+    block = models.ForeignKey(Block, on_delete = models.CASCADE)
+    number = models.IntegerField()
+    
+    image = models.ImageField(upload_to='floor_images/')
+
+    rows = models.IntegerField()
+    cols = models.IntegerField()
+
+    blocked_cells = models.JSONField(default=list)  # List of blocked cell coordinates
+
+    def __str__(self):
+        return f"{self.block.name} - Floor {self.number}"
+    def total_cells(self):
+        return self.rows * self.cols
+    
+
